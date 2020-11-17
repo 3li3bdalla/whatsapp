b5634bf452fcba0228356d7b1dce18b1cf8de271
aliabdalla <ali.dev.sd@gmail.com>
2020-11-17 20:34:56 +0300

    update package name

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '46f23d7e86a1902c2883a1d23a8b12bcb027b6bd',
+    'name' => '3li3bdalla/whatsapp',
+  ),
+  'versions' => 
+  array (
+    '3li3bdalla/whatsapp' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46f23d7e86a1902c2883a1d23a8b12bcb027b6bd',
+    ),
+  ),
+);
